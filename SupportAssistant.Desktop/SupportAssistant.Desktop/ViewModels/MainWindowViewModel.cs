95be63ca3bcdf2abf8ea1d4a4f6943ae5cf8b7c6
Nathan Miller <nmiller217@gmail.com>
2025-07-13 00:56:46 -0700

    rm projects

--- SupportAssistant.Desktop/SupportAssistant.Desktop/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SupportAssistant.Desktop.ViewModels;
-
-public class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- SupportAssistant.Desktop/SupportAssistant.Desktop/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace SupportAssistant.Desktop.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
